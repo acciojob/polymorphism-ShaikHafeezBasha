139e26ef6cfca897f7034f8c1d0040b8f04926c1
Hafeez Basha <shaikhafeez500@gmail.com>
2022-12-14 18:31:12 +0530

    Poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,23 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
+    class Product{
+        public void product(int x,int y){
+            System.out.println(x*y);
+        }
+        public void product(int x,int y,int z){
+            System.out.println(x*y*z);
+        }
+
+        public void product(double x,double y){
+            System.out.println(x*y);
+        }
+
+    }
+    public void main(String[] args) {
         Product p=new Product();
         p.product(5,6);
         p.product(5,6,7);
         p.product(5.1,6.2);
     }
-
-}
-class Product{
-    public void product(int x,int y){
-        System.out.println(x*y);
-    }
-    public void product(int x,int y,int z){
-        System.out.println(x*y*z);
-    }
-
-    public void product(double x,double y){
-        System.out.println(x*y);
-    }
-
 }
